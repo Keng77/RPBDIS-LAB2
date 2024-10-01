7a5fd2b63a3fc5cd426f66910f334e5f20c3364f
KENG ᅠ ᅠᅠ ᅠ <Keng7767@outlook.com>
2024-10-01 10:35:51 +0300

    Настроил работу с json

--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
-﻿public class Program
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RPBDISLAB2.Models;
+
+public class Program
 {
     static void Main(string[] args)
     {
-        
+        var builder = new ConfigurationBuilder();
+        // установка пути к текущему каталогу
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        // получаем конфигурацию из файла appsettings.json
+        builder.AddJsonFile("appsettings.json");
+        // создаем конфигурацию
+        var config = builder.Build();
+        // получаем строку подключения
+        string connectionString = config.GetConnectionString("MsSqlConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<InspectionsDbContext>();
+        var options = optionsBuilder
+            .UseSqlServer(connectionString)
+            .Options;
+
+        using (InspectionsDbContext db = new InspectionsDbContext(options))
+        {
+            //выполнение запросов к бд
+        }
+        Console.Read();
+
     }
+
+
 }
